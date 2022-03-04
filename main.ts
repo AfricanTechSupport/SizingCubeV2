4d9fea3a85de17a0c85f15e425f8099a7df396b6
Alan <obvicechief@gmail.com>
2022-03-04 13:10:42 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,130 @@
+function MediumCubeBuild () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . . . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . . . # .
+        . . # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . . . # .
+        . . . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . . . # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+function loadingScreen () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
+    basic.clearScreen()
+    basic.showString("Loading project. Please hold.")
+    basic.pause(5000)
+    basic.clearScreen()
+    basic.showString("Sizing Cube V2: loaded.")
+}
+function SmallCubeBuild () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
 function melodyPlay () {
     for (let index = 0; index < 100; index++) {
         music.playMelody("C D E D C D E F ", 150)
     }
 }
-basic.showLeds(`
-    # # # # #
-    # . . . #
-    # . # . #
-    # . . . #
-    # # # # #
-    `)
-basic.showLeds(`
-    . . . . .
-    . # # # .
-    . # . # .
-    . # # # .
-    . . . . .
-    `)
-basic.showLeds(`
-    # # # # #
-    # . . . #
-    # . # . #
-    # . . . #
-    # # # # #
-    `)
-basic.clearScreen()
-basic.showString("Loading project. Please hold.")
-basic.pause(5000)
-basic.clearScreen()
-basic.showString("Sizing Cube V2: loaded.")
-basic.forever(function () {
-    melodyPlay()
+function cubeBuild () {
+    BigCubeBuild()
+    MediumCubeBuild()
+    SmallCubeBuild()
+}
+function BigCubeBuild () {
     basic.showLeds(`
         # # # # #
         # . . . #
@@ -157,88 +251,9 @@ basic.forever(function () {
         . . . . .
         . . . . .
         `)
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . # # # .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . # # .
-        . # . # .
-        . # # # .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . # # .
-        . . . # .
-        . # # # .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . # # .
-        . . . # .
-        . . # # .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . # # .
-        . . . # .
-        . . . # .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . # # .
-        . . . # .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . # # .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+}
+loadingScreen()
+basic.forever(function () {
+    melodyPlay()
+    cubeBuild()
 })
